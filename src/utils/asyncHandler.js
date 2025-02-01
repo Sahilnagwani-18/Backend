d057e1bf6f7b7b2fea16382fbbba99bfa57b67af
Sahil <sahilnagwani121@gmail.com>
2025-02-01 15:35:05 +0530

    Commit latest

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+//Try Catch Wala
+// const asyncHandler = (fn) => async (req,res,next) =>{
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         });
+//     }
+// }
+
+//Now Promises=>
+const asyncHandler = (requestHandler) =>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err));
+    }
+}
+
+
+export {asyncHandler}
