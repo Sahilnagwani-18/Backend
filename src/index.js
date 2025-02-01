3f9e01dd2e8f865e371fded07f4ed39786f6a996
Sahil <sahilnagwani121@gmail.com>
2025-02-01 11:46:44 +0530

    Database Connection

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
+
+// reqiure('dotenv').config({path:"./env"})
+//This will also work But to remove Inconsistancy we use import wala statement 
+
+
+// Approach 1
+
+// import express from "express";
+
+// const app = express()
+// ;(async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}`)
+//        app.on("error",(error)=>{
+//         console.error("Error :",error);
+//         throw error;
+//        })
+
+//        app.listen(process.env.PORT,()=>{
+//          console.log(`App is Listening on Port ${process.env.PORT}`);
+//        })
+//     }catch(error){
+//         console.error("Error:",error);
+//         throw error
+//     }
+// }) ()
+
